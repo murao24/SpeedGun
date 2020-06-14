2d24dd591108a11c4575abb239d0a2daac3ec5a3
Yoshinobu <Yoshinobu>
2020-06-14 09:20:56 +0900

    first commit

---
+++ SpeedGun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedGun
+//
+//  Created by 村尾慶伸 on 2020/06/14.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
